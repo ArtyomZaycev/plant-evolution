31721a5506f6c46a8730e0d1384e0d7e5cd2ac3b
ArtyomZaycev <artem.zaycev.234@gmail.com>
2026-08-08 12:22:38 +0300

    Remove slow_mutex due to it being shit

--- src/lib/utils/mod.rs
+++ src/lib/utils/mod.rs
@@ -1,12 +1,10 @@
 mod shared_buffer;
-mod slow_mutex;
 mod stopwatch;
 mod types;
 mod versioned;
 mod versioned_mutex;
 
 pub use shared_buffer::*;
-pub use slow_mutex::*;
 pub use stopwatch::*;
 pub use types::*;
 pub use versioned::*;
