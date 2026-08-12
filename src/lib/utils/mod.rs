4ed9f5f9f082f055bc28efa6e628c86950634480
ArtyomZaycev <artem.zaycev.234@gmail.com>
2026-08-12 13:29:27 +0300

    Remove versioned_mutex

--- src/lib/utils/mod.rs
+++ src/lib/utils/mod.rs
@@ -2,10 +2,8 @@ mod shared_buffer;
 mod stopwatch;
 mod types;
 mod versioned;
-mod versioned_mutex;
 
 pub use shared_buffer::*;
 pub use stopwatch::*;
 pub use types::*;
 pub use versioned::*;
-pub use versioned_mutex::*;
