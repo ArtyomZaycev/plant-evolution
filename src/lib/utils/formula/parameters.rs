cabb8d99ca82003b79a5fe2ac44466b041320f5a
ArtyomZaycev <artem.zaycev.234@gmail.com>
2026-08-18 13:30:24 +0300

    Array as parameters

--- src/lib/utils/formula/parameters.rs
+++ src/lib/utils/formula/parameters.rs
@@ -78,3 +78,15 @@ pub trait ParameterId: Debug + Clone {
 pub trait Parameters<PId: ParameterId>: Debug {
     fn get_value(&self, id: &PId) -> f32;
 }
+
+impl ParameterId for usize {
+    fn get_name(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl Parameters<usize> for &[f32] {
+    fn get_value(&self, id: &usize) -> f32 {
+        self[*id]
+    }
+}
