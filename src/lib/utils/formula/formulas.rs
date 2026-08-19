dfb0a342fe867eeeaf2eca943769b2243697460b
ArtyomZaycev <artem.zaycev.234@gmail.com>
2026-08-19 09:59:09 +0300

    Fix boxed

--- src/lib/utils/formula/formulas.rs
+++ src/lib/utils/formula/formulas.rs
@@ -2,18 +2,31 @@ use std::fmt::Debug;
 
 use crate::utils::formula::{Formula, ParameterId, Parameters, TreeArrayFormula, TreeFormula};
 
-#[derive(Debug)]
-pub struct BoxedFormula<P: Debug> {
+pub struct BoxedFormula<P> {
     formula: Box<dyn Formula<P>>,
 }
 
-impl<P: Debug> ToString for BoxedFormula<P> {
+impl<P> BoxedFormula<P> {
+    pub fn new<F: Formula<P> + 'static>(formula: F) -> Self {
+        Self {
+            formula: Box::new(formula)
+        }
+    }
+}
+
+impl<P> Debug for BoxedFormula<P> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("BoxedFormula").field("formula", &self.formula).finish()
+    }
+}
+
+impl<P> ToString for BoxedFormula<P> {
     fn to_string(&self) -> String {
         format!("Boxed{}", self.formula.to_string())
     }
 }
 
-impl<P: Debug> Formula<P> for BoxedFormula<P> {
+impl<P> Formula<P> for BoxedFormula<P> {
     fn calculate(&self, parameters: &P) -> f32 {
         self.formula.calculate(parameters)
     }
